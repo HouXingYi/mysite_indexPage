bedb2054a1194b5b1387a63ca0d170d5589ace83
houxingyi <1026043046@qq.com>
2017-12-17 10:48:20 +0800

    save

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 8082;
+var port = 8083;
 app.use("/",express.static(__dirname + "/public"));
 app.listen(port);
 console.log("app is runing on port"+port);
