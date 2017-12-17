9d03a42c300dfe7b4ae6b1c6fcafe43eadae7ad7
houxingyi <1026043046@qq.com>
2017-12-17 10:44:52 +0800

    add deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [
+    {
+      name      : 'indexPage',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+  deploy : {
+    production : {
+      user : 'houxingyi',
+      host : '47.254.26.35',
+      ref  : 'origin/master',
+      repo : 'git@github.com:HouXingYi/mysite_indexPage.git',
+      path : '/www/indexPage',
+      ssh_options: "StrictHostKeyChecking=no",
+      env: {
+          "NODE_NEV": "production"
+      }
+    },
+  }
+};
